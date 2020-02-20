1c2fcd700c5409df432bbeec7d6466655425eac1
johnnguyen-nodejs <hotro.dacsanhuousao@gmail.com>
2020-02-20 08:11:05 +0700

    v3

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,27 @@ var initRouter = (app)=>{
 
     router.get('/',(req,res)=>{
         res.render("index",{
-            title: "TULPO | Home"
+            title: "TULPO | Trang Chủ"
+        })
+    })
+    router.get('/about',(req,res)=>{
+        res.render("about",{
+            title: "TULPO | Giới thiệu"
+        })
+    })
+    router.get('/process',(req,res)=>{
+        res.render("process",{
+            title: "TULPO | Quy Trình Làm Việc"
+        })
+    })
+    router.get('/contact',(req,res)=>{
+        res.render("contact",{
+            title: "TULPO | Liên Hệ"
+        })
+    })
+    router.get('/price',(req,res)=>{
+        res.render("price",{
+            title: "TULPO | Báo Giá Sản Phẩm"
         })
     })
 
